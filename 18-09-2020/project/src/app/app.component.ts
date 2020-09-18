cabbdc61548444c6c69012462606a20d9b6ff3e2
Sourav <souravshit.97@gmail.com>
2020-09-18 19:15:57 +0530

    Angular-10 file uploded

---
+++ 18-09-2020/project/src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  
+data=[
+  {
+    name:"Sourav",
+    age:"23"
+  },
+  {
+    name:"Raju",
+    age:"22"
+  },
+  {
+    name:"Suman",
+    age:"24"
+  }
+
+]
+}
+
